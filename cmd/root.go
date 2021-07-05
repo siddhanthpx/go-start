1a276d440bda060b648e1e58a40268086c0380b6
siddhanthpx <siddhanth0@outlook.com>
2021-07-05 22:43:56 +0530

    formatted

--- cmd/root.go
+++ cmd/root.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+
 	"github.com/spf13/cobra"
 
 	"github.com/spf13/viper"
@@ -28,13 +29,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-start",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "CLI tool for generating go application scaffolding.",
+	Long: `go-start is a simple automation tool that generates boiler plate (go.mod , main.go, .git)
+and even initializes a GitHub repository (login required).`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
